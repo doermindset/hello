9efeb1685c428763ce58011af299e33ef15917c6
CaesarHD <raul.stefan.pop@stud.ubbcluj.ro>
2022-10-04 11:38:23 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+        float inc = 0;
+        float sum = 0;
+        System.out.println("Number: ");
+        float num = in.nextInt();
+        while(num != 0){
+            System.out.println("Number: ");
+            sum += num;
+            inc++;
+            num = in.nextInt();
+        }
+        float avg = sum / inc;
+        System.out.println("Average: " + avg);
     }
 }
